bc06ceba2a32d4be54dd21eef743de66be0e51c7
vicram10 <vicram_42@hotmail.com>
2019-08-12 02:05:27 +0000

    modificamos algo para tener lo ultimo desde el imac

--- contents/languages/principal.spanish.php
+++ contents/languages/principal.spanish.php
@@ -7,4 +7,4 @@
     const page_title_login = 'Iniciar Sesi&oacute;n';
     const label_usuario = 'Usuario';
     const label_password = 'Contrase&ntilde;a';
-    const mensaje_iniciado_correctamente = 'Inicio de Sesion de forma correcta.';
+    const mensaje_iniciado_correctamente = 'Inicio de Sesi&oacute;n de forma correcta.';
