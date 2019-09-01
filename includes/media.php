1e7b2be9807d762b81800333f09480fc6a97f271
vicram10 <vicram_42@hotmail.com>
2019-09-01 12:05:36 +0000

    funciona subida, vamos a modificar el css

--- includes/media.php
+++ includes/media.php
@@ -4,7 +4,7 @@ if (!defined('eGeek')) die ('Acceso prohibido');
 //ok ya enviamos para subir la imagen
 if (!empty($_FILES)) {
     $tempFile = $_FILES['file']['tmp_name'];
-    $targetPath = DirContent . '/themes/default/images/';
+    $targetPath = DirContent . '/upload/';
     $targetFile =  $targetPath. $_FILES['file']['name'];
     move_uploaded_file($tempFile,$targetFile);
 }
